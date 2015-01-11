711a3262d42ab650cb1eb98aa04bdb377df45600
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2015-01-11 12:44:01 +0100

    Fixer SourFile class + some tests

--- TestRead.scala
+++ TestRead.scala
@@ -4,8 +4,14 @@ import scala.collection.mutable.Set
 import scala.sys.process._
 
 object TestRead {
-  // helper class to handle a target's command and all its dependencies
-  class SourceTuple(var _1: String, var _2: Set[String], var cmds: List[String]) {}
+  /**
+   * Helper class to handle a target's command and all its dependencies
+   * @member name Actual target name
+   * @member deps List of target dependencies
+   * @member cmds List of commands that need to be executed. They are in
+   * reverse order. You should pop the back of it
+   */
+  class SourceTuple(var name: String, var deps: Set[String], var cmds: List[String]) {}
   def main(args: Array[String]) {
     val fi = Source.fromFile(if (args.length < 1) "testmakefile" else args(0))
     var tabs, lines = 0
@@ -15,7 +21,6 @@ object TestRead {
      * exist) and a list of its dependencies as Strings
      */
     var files: Map[String, SourceTuple] = Map()
-    var cmds: Map[String, String] = Map()
     var lastTarget = ""
     for (line <- fi.getLines()) {
       if (line.indexOf(':') > 0) { // new target here
@@ -25,24 +30,24 @@ object TestRead {
         if (files contains lastTarget)
           println("WARNING: double reference to "+lastTarget+". Ignoring...")
         else // add the entry with its dependencies (if they exist)
-          files += (lastTarget -> new SourceTuple("",
+          files += (lastTarget -> new SourceTuple(lastTarget,
             if (splitted.length > 1) Set(splitted(1).replaceAll("[ \t]", " ").split(' ').filter(_ != "") :_*) else Set(),
             List[String]()
             ))
       }
       if (line.length > 0 && line(0) == '\t' && lastTarget != "") {
         tabs += 1
-        files(lastTarget)._1 = line.replaceAll("^\t+", "") // XXX Support only one command
-        println("Reading command for "+lastTarget+":"+files(lastTarget)._1)
-        cmds += (lastTarget -> files(lastTarget)._1)
-        files(lastTarget).cmds = files(lastTarget)._1 :: files(lastTarget).cmds
+        files(lastTarget).cmds = line.replaceAll("^\t+", "") :: files(lastTarget).cmds // XXX Support only one command
       }
       lines += 1
     }
 
     //for ((key, value) <- files) {
-      //println("Target "+key+" has "+value._2.size+" deps before loop");
+      //println("Target "+key+" has "+value.deps.size+" deps before loop");
     //}
+
+    // TODO Check for files that exists and remove them from dependencies or
+    // add a special attribute to say that it is a exisiting file
 
     // now expand dependencies
     var check = new Array[String](0)
@@ -51,16 +56,16 @@ object TestRead {
         var deps = Stack[String]() // contains every file that need to be checked
         var visited = Stack[String]() // keep trace of visited nodes
         // init stack with already known deps
-        for (file <- value._2) {
+        for (file <- value.deps) {
           deps.push(file)
         }
         while (deps.length > 0) {
           val dep = deps.pop()
           if (visited.indexOf(dep) < 0) {
             visited.push(dep)
-            value._2 += dep // store our dependency
+            value.deps += dep // store our dependency
             if (files contains dep) { // check for further dependencies
-              val dep_deps = files(dep)._2
+              val dep_deps = files(dep).deps
               for (dep_dep <- dep_deps) {
                 if (visited.indexOf(dep_dep) < 0) { // add them if they have not been checked
                   deps.push(dep_dep)
@@ -73,11 +78,10 @@ object TestRead {
     }
 
     for ((key, value) <- files) {
-      println("Target "+key+" has "+value._2.size+" deps"+ (if (value._2.size > 0) ": "+value._2 else ""));
-      println("Command to execute: "+value.cmds)
+      println("Target "+key+" has "+value.deps.size+" deps"+ (if (value.deps.size > 0) ": "+value.deps else ""));
+      println(value.cmds.length + " commands to execute: "+value.cmds)
       //Using full call to not mess up with pipes and others
-      sys.process.stringSeqToProcess(Seq("/bin/bash","-c",cmds(key)))!
-      //sys.process.stringSeqToProcess(Seq("/bin/bash","-c",value.cmds(0)))!
+      sys.process.stringSeqToProcess(Seq("/bin/bash","-c",value.cmds(0)))!
     }
   }
 }
